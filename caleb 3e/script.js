d2ff6524b1e5fbe14306b73e643a09dd179e8989
Calebelopesnoletosilva <lopesscalebe858@gmail.com>
2025-04-02 21:03:58 -0300

    Add files via upload

---
+++ caleb 3e/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+const botaDeAcessesibilidade=document.getElementByid("botao-acessibilidade");
+const opcoeDeAcessibilidade=document.getElementById("opcoes-acessibilidade")
+
+botaoDeAcessibilidade.addEventListener('click',funcition(){
+
+
+
+
+
